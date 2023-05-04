d4f21d703529f2329e4b5622c9e5f9a4ee66195c
HammerTooth <arthur.frebourg@gmail.com>
2023-05-04 09:29:04 +0200

    Configured app for deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,9 @@ module.exports = {
     {
       name: "app",
       script: "./www/app.js",
+      max_memory_restart: "200M",
+      error_file: "./logs/err.log",
+      instances: 3,
       env_production: {
         NODE_ENV: "production",
       },
